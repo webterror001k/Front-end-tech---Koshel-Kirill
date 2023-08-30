237ae8c7b79d541ad1fb1bf40516cad222701751
webterror001k <90241180+webterror001k@users.noreply.github.com>
2023-08-30 22:39:21 +0300

    Connected copy-webpack-plugin and configured webpack

--- lab3/webpack-project/webpack.config.js
+++ lab3/webpack-project/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     entry: {
@@ -12,15 +13,33 @@ module.exports = {
     },
     plugins: [
         new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns:[{
+                from: 'src/assets/images', 
+                to: 'images',
+                noErrorOnMissing: true,
+            }],
+        }),
         new HtmlWebpackPlugin({
             title: 'webpack index.html',
             filename: 'index.html',
             template: path.resolve(__dirname, './src/pages/index.html'),
         }),
         new HtmlWebpackPlugin({
-            title: 'webpack about.html',
-            filename: 'about.html',
-            template: path.resolve(__dirname, './src/pages/about.html'),
+            title: 'webpack news.html',
+            filename: 'news.html',
+            template: path.resolve(__dirname, './src/pages/news.html'),
         }),
+        new HtmlWebpackPlugin({
+            title: 'webpack photo.html',
+            filename: 'photo.html',
+            template: path.resolve(__dirname, './src/pages/photo.html'),
+        }),
+        new HtmlWebpackPlugin({
+            title: 'webpack rozklad.html',
+            filename: 'rozklad.html',
+            template: path.resolve(__dirname, './src/pages/rozklad.html'),
+        }),
+        
     ],
 }
